3ba2d7e0b3b0b56a57726a48b2b07f5b5f4464e7
unknown <rajeshbhadange2001@gmail.com>
2023-09-26 23:04:10 +0530

    MathOperation

---
+++ mathOperation.java
@@ -0,0 +1,19 @@
+public class mathOperation {
+    public static void main(String[] args) {
+        // Define variables for numbers
+        double num1 = 10;
+        double num2 = 5;
+
+        // Perform addition, subtraction, multiplication, and division
+        double sum = num1 + num2;
+        double difference = num1 - num2;
+        double product = num1 * num2;
+        double quotient = num1 / num2;
+
+        // Display the results
+        System.out.println("Addition: " + sum);
+        System.out.println("Subtraction: " + difference);
+        System.out.println("Multiplication: " + product);
+        System.out.println("Division: " + quotient);
+    }
+}
